a771d98e77398b0bb300ac829cc34ea4a66a0292
Giulio Rossi <ciuffi9@mac.com>
2019-12-11 15:30:21 -0800

    add database_handler

---
+++ src/database_handler.py
@@ -0,0 +1,37 @@
+import pymongo
+
+from pymongo.collection import Collection
+from pymongo.database import Database
+from pymongo.cursor import Cursor
+from datetime import datetime
+class database_handler():
+    client: pymongo.MongoClient
+    COLLECTION_NAME = 'runs'
+    DATABASE_NAME = 'brc'
+    db: Database
+    collection: Collection
+    def __init__(self, db_string):
+        self.client = pymongo.MongoClient(db_string)
+        self.db = self.client[self.DATABASE_NAME]
+        self.collection = self.db[self.COLLECTION_NAME]
+    
+    def record_run(self, run_name: str, processed: bool, error: bool) -> None:
+        """Record a new run. All paremeters are required """
+        self.collection.insert_one({
+            "name": run_name,
+            "processed": processed,
+            "error": error,
+            "created": datetime.utcnow()
+        })
+    
+    def get_run_names(self) -> Cursor:
+        """Return the Name and Ids of all runs"""
+        return self.collection.find({}, {"name": 1, "_id": 1})
+
+    def get_last_run(self) -> dict:
+        """Get the last/current run"""
+        return self.collection.find({}).sort([("_id", pymongo.DESCENDING)]).limit(1)[0]
+
+    def updateRun(self, id, **kwargs):
+        """Update a run with new data"""
+        return self.collection.update_one({"_id": id}, {"$set": kwargs})
